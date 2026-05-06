f9a57bc49772743ad0b4f3cc148392983011e5ee
Catarina2311 <catarina.maria@aluno.ifsp.edu.br>
2026-05-06 14:00:03 -0300

    Add files via upload

---
+++ crocodaily/model/socialModel.py
@@ -0,0 +1,10 @@
+class SocialModel:
+    def __init__(self, avaliacao):
+        self.__ = avaliacao
+
+    def getSocial(self):
+        return(self.__avaliacao)
+    
+    def setSocial(self, avaliacao):
+        self.__avaliacao = avaliacao
+        
